13b4d1d5513fd568bdde5efdb4b4af57a181f0d2
shabazbaig5 <shabazbaig5@gmail.com>
2019-09-11 20:31:33 +0530

    Update diagonal_Difference.js

--- code/diagonal_Difference.js
+++ code/diagonal_Difference.js
@@ -6,30 +6,7 @@
 //  */
 
 function diagonalDifference(arr) {
-    
-    
-    let num = 27;
-let div;
-let bin = [];
-while(num > 1){
-
-  div = parseInt(num/2);
-  
-  if(num%2 == 0 || num%2==1){
-    bin.push(num%2);
-
-    
-    // console.log(num%2);
-  }
-
-  num = div;
-
-  
-
-
-}
-bin.push(1);
-console.log(bin);
+   
     // Write your code here
 
     // console.log(arr[0]);
