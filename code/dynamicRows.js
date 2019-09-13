241d723500283b2a4b4b7fa15f28002de0e46c82
Ethan Hunt <shabazbaig5@gmail.com>
2019-09-13 14:55:56 +0530

    working logic for adding elements dynamically

--- code/dynamicRows.js
+++ code/dynamicRows.js
@@ -1,106 +1,50 @@
 
-// let dynamicDivs = document.querySelector('.dynamicDivs');
-
-// let div = dynamicDivs.querySelector('div');
-
-// console.log(div.childNodes);
-
-// console.log(div);
-
-
-// let addDivBtn = document.querySelector('.addDiv');
-
-
-
-
-// addDivBtn.addEventListener("click", (e) => {
-
-
-//   let nextDiv = div.cloneNode(true);
-
-//   console.log("The next div is");
-//   console.log(nextDiv);
-//   let addBtn = nextDiv.querySelector('.addDiv');
-
-//   // e.target.setAttribute("onclick", alert('hey'));
-
-//   addBtn.classList.add('addIt');
-
-  
-
-  
-//   dynamicDivs.appendChild(nextDiv);
-
-
-//   // addBtn.setAttribute("onclick", alert("heyyy"));
-
-
-//   document.querySelectorAll('.dynamicDivs div').forEach((div) => {
-
-//     console.log(div.querySelector('button'));
-
-//     div.querySelector('button').addEventListener("click", () => {
-//       console.log('hey clicked');
-//       let nextDiv = div.cloneNode(true);
-
-//       console.log("The next div is");
-//       console.log(nextDiv);
-//       let addBtn = nextDiv.querySelector('.addDiv');
-    
-//       // e.target.setAttribute("onclick", alert('hey'));
-    
-//       addBtn.classList.add('addIt');
-    
-      
-    
-      
-//       dynamicDivs.appendChild(nextDiv);
-//       // console.log(e.target);
-//       // addTheEvent();
-
-//     })
-
-//   })
-//   console.log(dynamicDivs);
-
-  
-
-
-// });
-
-
-// console.log(dynamicDivs.childNodes[1]);
-
-
-
-
 let parentDiv = document.querySelector('.dynamicDivs');
-
-console.log(parentDiv);
-console.log(parentDiv.querySelectorAll('div'));
 
 let divs = parentDiv.querySelectorAll('div');
 
 let lastDiv = divs[divs.length - 1];
-console.log(lastDiv.querySelector('button'));
+let addBtn = lastDiv.querySelector('button');
+let removeBtn = lastDiv.childNodes[lastDiv.childNodes.length - 2];
+console.log(removeBtn);
 
-lastDiv.addEventListener("click", () => {
 
-  addTheDiv();
+// let body = document.querySelector('body');
 
+parentDiv.addEventListener('click', (e) => {
+
+  
+  if(e.target == addBtn){
+
+    console.log("Clicked the last btn");
+    console.log(e.target);
+    console.log(addBtn);
+
+    addTheDiv();
+
+  }else if(e.target == removeBtn){
+
+    console.log("Clicked on the remove btn");
+    console.log(removeBtn);
+
+  }
 });
+
+
+
+
+
 
 addTheDiv = () => {
 
-  console.log("Clicked the last one");
   let nextDiv = document.createElement('div');
-
   nextDiv = lastDiv.cloneNode(true);
-
   parentDiv.appendChild(nextDiv);
-
+  addBtn = nextDiv.querySelector('button');
+  console.log(addBtn)
 
 }
+
 
 
 
